6178d741c9d3b6641afe2d0fecc83efe8380de2b
Thomas Geissl <thomas.geissl@gmail.com>
2019-12-16 23:10:10 +0100

    added gui

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -2,31 +2,48 @@
 
 void ofApp::setup()
 {
+    drawGui = true;
     ofBackground(0);
     ofSetFrameRate(60);
-    
+
     // Setup post-processing chain
     post.init(ofGetWidth(), ofGetHeight());
-    post.createPass<FxaaPass>()->setEnabled(false);
-    post.createPass<BloomPass>()->setEnabled(false);
-    post.createPass<DofPass>()->setEnabled(false);
-    post.createPass<KaleidoscopePass>()->setEnabled(false);
-    post.createPass<NoiseWarpPass>()->setEnabled(false);
-    post.createPass<PixelatePass>()->setEnabled(false);
-    post.createPass<EdgePass>()->setEnabled(false);
-    post.createPass<VerticalTiltShifPass>()->setEnabled(false);
-    post.createPass<GodRaysPass>()->setEnabled(false);
-    
+    effects.push_back(post.createPass<ContrastPass>());
+    effects.push_back(post.createPass<HsbShiftPass>());
+    effects.push_back(post.createPass<RGBShiftPass>());
+    effects.push_back(post.createPass<LimbDarkeningPass>());
+    effects.push_back(post.createPass<FxaaPass>());
+    effects.push_back(post.createPass<FakeSSSPass>());
+    effects.push_back(post.createPass<BloomPass>());
+    effects.push_back(post.createPass<DofPass>());
+    effects.push_back(post.createPass<KaleidoscopePass>());
+    effects.push_back(post.createPass<NoiseWarpPass>());
+    effects.push_back(post.createPass<PixelatePass>());
+    effects.push_back(post.createPass<EdgePass>());
+    effects.push_back(post.createPass<HorizontalTiltShifPass>());
+    effects.push_back(post.createPass<VerticalTiltShifPass>());
+    effects.push_back(post.createPass<GodRaysPass>());
+    effects.push_back(post.createPass<SSAOPass>());
+    effects.push_back(post.createPass<ToonPass>());
+    effects.push_back(post.createPass<ZoomBlurPass>());
+    gui.setup("gui", "settings.xml", 20, 20);
+    for (auto &effect : effects)
+    {
+        effect->setEnabled(false);
+        gui.add(effect->parameters);
+    }
+    gui.minimizeAll();
+
     // Setup box positions
     for (unsigned i = 0; i < NUM_BOXES; ++i)
     {
         posns.push_back(ofVec3f(ofRandom(-300, 300), ofRandom(-300, 300), ofRandom(-300, 300)));
         cols.push_back(ofColor::fromHsb(255 * i / (float)NUM_BOXES, 255, 255, 255));
     }
-    
+
     // Setup light
-	light.setPosition(1000, 1000, 2000);
-    
+    light.setPosition(1000, 1000, 2000);
+
     // create our own box mesh as there is a bug with
     // normal scaling and ofDrawBox() at the moment
     boxMesh = ofMesh::box(20, 20, 20);
@@ -39,13 +56,14 @@ void ofApp::update()
 
 void ofApp::draw()
 {
+
     // setup gl state
     ofEnableDepthTest();
     light.enable();
-    
+
     // begin scene to post process
     post.begin(cam);
-    
+
     // draw boxes
     for (unsigned i = 0; i < posns.size(); ++i)
     {
@@ -55,27 +73,25 @@ void ofApp::draw()
         boxMesh.draw();
         ofPopMatrix();
     }
-    
-    ofDrawAxis(100);
-    
+
+    // ofDrawAxis(100);
+
     // end scene and draw
     post.end();
-    
-    // draw help
-    ofSetColor(0, 255, 255);
-    ofDrawBitmapString("Number keys toggle effects, mouse rotates scene", 10, 20);
-    for (unsigned i = 0; i < post.size(); ++i)
+
+    if (drawGui)
     {
-        if (post[i]->getEnabled()) ofSetColor(0, 255, 255);
-        else ofSetColor(255, 0, 0);
-        ostringstream oss;
-        oss << i << ": " << post[i]->getName() << (post[i]->getEnabled()?" (on)":" (off)");
-        ofDrawBitmapString(oss.str(), 10, 20 * (i + 2));
+        ofDisableDepthTest();
+        gui.draw();
     }
 }
 
 void ofApp::keyPressed(int key)
 {
-    unsigned idx = key - '0';
-    if (idx < post.size()) post[idx]->setEnabled(!post[idx]->getEnabled());
+    switch (key)
+    {
+    case 'g':
+        drawGui = !drawGui;
+        break;
+    }
 }
--- example/src/ofApp.h
+++ example/src/ofApp.h
@@ -1,26 +1,30 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
 #include "ofxPostProcessing.h"
 
 class ofApp : public ofBaseApp
 {
 public:
     static const unsigned NUM_BOXES = 100;
-    
+
     void setup();
     void update();
     void draw();
 
     void keyPressed(int key);
-    
+
     // scene stuff
     ofxPostProcessing post;
+    std::vector<RenderPass::Ptr> effects;
     ofEasyCam cam;
     ofLight light;
-    
+
     // boxes
     vector<ofVec3f> posns;
     vector<ofColor> cols;
     ofVboMesh boxMesh;
+    ofxPanel gui;
+    bool drawGui;
 };
